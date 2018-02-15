a8e0cd073ccbb1042ea3bbf68c51216c76ba9f99
Unknown <diidiie159@gmail.com>
2018-02-15 14:53:34 +0100

    Scraping Michelin ok

--- docs/js/michelin.js
+++ docs/js/michelin.js
@@ -13,25 +13,68 @@ var cheerio = require('cheerio');
 function get() {
 
     url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
-
-    request(url, function(error, response, html){
+	let restaurants = [];
+	page(url);
+    function page(url) {
+	let ct = 0;
+	request(url, function(error, response, html) {
         if(!error){
-            var $ = cheerio.load(html);
-
-            var title;
-            var json = { title : ""};
-//essayons de boucler plus large, pour pouvoir avoir plus facilement le nombre d'etoile et l'adresse
-            $('.poi_card-display-title').filter(function(){ //on met le nom du truc que l'on veut
+            let $ = cheerio.load(html);
+            $('.poi-card-link').filter(function() {
                 var data = $(this);
-                title = data.text();
-				console.log(title);
-                json.title = title;
-            })
+                let webAddress = data.attr('href');
+				ct ++;
+				request('https://restaurant.michelin.fr'+webAddress, function(error, response, html) {
+					if(!error){
+						let $ = cheerio.load(html);
+						let restaurant = {};
+						$('.poi_intro-display-title').filter(function() {
+							let resto = $(this);
+							restaurant.name = resto.text().trim();
+						});
+						
+						$('.street-block').filter(function() {
+							let add = $(this);
+							restaurant.rue = add.text();
+						});
+						
+						$('.locality').filter(function() {
+							let loc = $(this);
+							restaurant.ville = loc.text();
+						});
+						
+						$('.postal-code').filter(function() {
+							let pc = $(this);
+							restaurant.code_postal = pc.text();
+						});
+						
+						restaurants.push(restaurant);
+					}
+					ct --;
+					if(ct == 0) {
+						fs.writeFile('output.json', JSON.stringify(restaurants), function(err){});
+						//console.log(restaurants);
+					}
+				});
+            });
+			
+			$('.item-list-first div ul .last span').filter(function() {
+				console.log('opening next page');
+				var next = $(this);
+                console.log(next.attr('attr-page-number'));
+				let nextUrl = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-' + next.attr('attr-page-number');
+				if(nextUrl != url) {	
+					page(nextUrl);
+				}
+			});
+			
         }
-fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){})	
-    })
+    });	
 }
-
+}
 module.exports.get = get;
+
+
+	
 
 
