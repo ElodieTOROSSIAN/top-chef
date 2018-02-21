5aa6225b63668138d8a7176fca79e77e94b7ece1
Unknown <diidiie159@gmail.com>
2018-02-21 10:44:25 +0100

    Essais :(
    Je comprend pas pk ca marche pas 
    A revoir

--- docs/js/lafourchette.js
+++ docs/js/lafourchette.js
@@ -6,16 +6,20 @@ function getDeal() {
 	urlrecherhce = 'https://www.lafourchette.com/search-refine/';
 	url = 'https://www.lafourchette.com/';
 	
-	var datafile = fs.readFileSync('C:/Users/elodi/Documents/Cours/ESILV/A4/S2/Web/TD3/top-chef/docs/js/output.json', "utf8");
-	var resto = JSON.parse(datafile);
-	request('https://www.lafourchette.com/search-refine/agap%C3%A9', function(error, response, html) {
+	let datafile = fs.readFileSync('C:/Users/elodi/Documents/Cours/ESILV/A4/S2/Web/TD3/top-chef/docs/js/output.json', "utf8");
+	let resto = JSON.parse(datafile);
+	request('https://www.lafourchette.com/', function(error, response, html) {
 		if(!error){
 			let $ = cheerio.load(html);
+			console.log(html);
 			$('.resultItem-name a').filter(function() {
-				let add = $(this).children;
-				console.log(add);
-			//on veut essayer de rentrer dans l'element et ensuite tester avec l'adesse si c'est le bon et cela pour chaque element 
-		});
+				let add = $(this).attr('href');
+				console.log('add'); //JE COMPREND PAS POURQUOI CA AFFICHE RIEN :(
+				//ensuite on rentre dedans
+				//on verifie l'adresse
+				//on prend le bon 
+				//on passe au suivant
+			});
 		}
 	});
 	
